7991c760d5d815ca8a1dc1744f289fc5e9150f2a
Tuvio717 <46975220+MatteoTuvioGarcia@users.noreply.github.com>
2022-03-03 17:03:43 +0100

    çiojuojui

--- phpbdd/php_acces_db_03/include/executerequete.php
+++ phpbdd/php_acces_db_03/include/executerequete.php
@@ -1,9 +1,9 @@
 <?php
-function executerequete($cnx, $born =[]){
-    $varQuery = 'SELECT * FROM auteur WHERE date_naissance BETWEEN ? AND ?';
+function executerequete($cnx,$req, $born =[]){
+
 
     // 4 - preparation de la requête et retourne un identifiant de connexion
-    $idRequete = $cnx->prepare($varQuery);
+    $idRequete = $cnx->prepare($req);
     // Execution de la requete avec les parametres
     $idRequete->execute($born);
 
--- phpbdd/php_acces_db_03/sProgramme.php
+++ phpbdd/php_acces_db_03/sProgramme.php
@@ -1,9 +1,9 @@
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="utf-8">
-    <link href=https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href=https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Acces BD avec PDO</title>
 </head>
 <body>
@@ -17,27 +17,28 @@
             <?php
             // Utilisation de PDO : PHP DATA OBJECT
 
-//import des scripts
-            if(isset($_POST['btn_choix'])) {
+            //import des scripts
+            if (isset($_POST['btn_choix'])) {
                 require_once 'include/infoconnection.php';
                 require_once 'include/connection.php';
                 require_once 'include/executerequete.php';
-            $min = $_POST['min'];
-            $max=$_POST['max'];
-            $cnx=connexion();
-            $idRequete=executerequete($cnx,[$min,$max]);
-                while($row = $idRequete->fetch(PDO::FETCH_ASSOC)){
+                $min = $_POST['min'];
+                $max = $_POST['max'];
+                $cnx = connexion();
+                $req='SELECT * FROM auteur WHERE date_naissance BETWEEN ? AND ?';
+                $idRequete = executerequete($cnx,$req, [$min, $max]);
+                while ($row = $idRequete->fetch(PDO::FETCH_ASSOC)) {
                     echo $row['id_auteur'] . ' / ' . $row['nom'] . '<br>';
                 }
-            // 6 - Fermeture de la connexion
-            $cnx = null;
+                // 6 - Fermeture de la connexion
+                $cnx = null;
             }
             ?>
-        <form method="post" action="sProgramme.php">
-            <input type="number" name="min"/>
-            <input type="number" name="max"/>
-            <input type="submit" name="btn_choix"/>
-        </form>
+            <form method="post" action="sProgramme.php">
+                <input type="number" name="min"/>
+                <input type="number" name="max"/>
+                <input type="submit" name="btn_choix"/>
+            </form>
         </div>
 
 
