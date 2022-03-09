e9c5264171ed80b392d203c7f7fd352b3455c8d0
Tuvio717 <46975220+MatteoTuvioGarcia@users.noreply.github.com>
2022-03-09 10:00:32 +0100

    Update sAssure.php

--- PHP-POO/MiniTP/sAssure.php
+++ PHP-POO/MiniTP/sAssure.php
@@ -4,7 +4,7 @@ require_once "assure.php";
 //création d'une instance de classe
 $objPierre = new Assure("Pierre", "Déols", 22);
 $objJulie = new Assure("Julie", "Paris", 21);
-
+const BR = "<br>";
 $objPierre->reglerassurance();
 $objPierre->parrainer($objJulie);
 $objJulie->avoiraccident();
@@ -12,5 +12,5 @@ $objJulie->reglerassurance();
 $objPierre->setPointsFidelite(20);
 $objPierre->setBonusmalus(20);
 $objPierre->reglerassurance();
-echo $objPierre->getNom() . " Points de fidelite: " .$objPierre->getPointsFidelite() . " Bonusmalus: " . $objPierre->getBonusMalus() . "<br>";
-echo $objJulie->getNom() . " Points de fidelite: " .$objJulie->getPointsFidelite() . " Bonusmalus: " . $objJulie->getBonusMalus();
+echo $objPierre->getNom() . " Points de fidelite: " .$objPierre->getPointsFidelite() . " Bonusmalus: " . $objPierre->getBonusMalus() . BR;
+echo $objJulie->getNom() . " Points de fidelite: " .$objJulie->getPointsFidelite() . " Bonusmalus: " . $objJulie->getBonusMalus(). BR;
